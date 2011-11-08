4c5bbf999c81aafd209d463aee4f9c664503fa4a
mattiuzzi5114 <mattiuzzi5114@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-08 12:15:11 +0000

    added zzz.R file (copied and addapted from raster packge)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modis@14 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/MODIS/R/zzz.R
@@ -0,0 +1,9 @@
+# zzz.R taken from raster package
+.onLoad <- function(lib, pkg)  {
+	pkg.info <- drop(read.dcf(file=system.file("DESCRIPTION", package=pkg), fields=c("Version","Date")))
+	packageStartupMessage(paste("\n",pkg, " version ", pkg.info["Version"], " (", pkg.info["Date"], ")", sep=""))
+	tst <- try( removeTmpFiles(), silent=TRUE )
+
+	return(invisible(0))
+}
+
