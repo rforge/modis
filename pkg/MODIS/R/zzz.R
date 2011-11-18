e5b2507b267b2f6d55e270f588967e5ba390afe8
mattiuzzi5114 <mattiuzzi5114@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-18 13:03:18 +0000

    link to pdf in load message
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modis@22 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/MODIS/R/zzz.R
+++ pkg/MODIS/R/zzz.R
@@ -1,7 +1,7 @@
 # zzz.R taken from raster package
 .onLoad <- function(lib, pkg)  {
 	pkg.info <- drop(read.dcf(file=system.file("DESCRIPTION", package=pkg), fields=c("Version","Date")))
-	packageStartupMessage(paste("\n",pkg, " version ", pkg.info["Version"], " (", pkg.info["Date"], ")\nThe MODIS package is still in an early development status. So bugs and substanzial changes are very frequent!\n", sep=""))
+	packageStartupMessage(paste("\n",pkg, " version ", pkg.info["Version"], " (", pkg.info["Date"], ")\nThe MODIS package is still in an early development status. So bugs and substanzial changes are very frequent!\nMODIS_manual: http://www.wuala.com/IVFL/R_MODIS/ ", sep=""))
 	tst <- try( removeTmpFiles(), silent=TRUE )
 
 	return(invisible(0))
