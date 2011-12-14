93c0eed5f0e3d567d139ceba2f92dff6ab59c9e9
mattiuzzi5114 <mattiuzzi5114@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-14 15:59:02 +0000

    getSDS default method is now gdal (gdal can be expected to be awailable on the system...mrt not! A step to remove dependency from "mrt")
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modis@37 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/MODIS/R/getSDS.R
+++ pkg/MODIS/R/getSDS.R
@@ -1,12 +1,12 @@
-getSDS <- function(HdfName,SDSstring,MRTpath="check") {
+# Author: Matteo Mattiuzzi, matteo.mattiuzzi@boku.ac.at
+# Date : August 2011
+# Licence GPL v3
+
+getSDS <- function(HdfName,SDSstring,method="gdal",...) {
 ######
+
+method <- tolower(method) 
 fsep <- .Platform$file.sep
-
-if (MRTpath=="check") {
-	MRTpath <- getPATH(quiet=TRUE)
-	}
-
-if (!file.exists(MRTpath)) {stop("'MRTpath' is wrong or MRT not installed? Provide a good path, leave empty or run 'getPATH()' first!")}
 
 if (!file.exists(HdfName)) {
 	cat("Hm, I have to search for the file! Next time provide the full path and I'll be very fast!\n")
@@ -14,24 +14,53 @@ if (!file.exists(HdfName)) {
 	}
 	
 	HdfName <- HdfName[1]
+
+if (method=="gdal"){
+
+	if (.Platform$OS=="unix"){
+		sdsRaw <- system(paste("gdalinfo ", HdfName,sep=""),intern=TRUE) 
 	
-if (.Platform$OS=="unix"){
+	} else if (.Platform$OS=="windows"){
+		sdsRaw <- shell(paste("gdalinfo ", HdfName,sep=""),intern=TRUE)
+
+	} else {
+		stop(cat("What OS have you? Please tell me so I can fix this.\n")) 
+	}
+
+	SDSnames <- grep(x=sdsRaw,pattern="SUBDATASET_*.._NAME",value=T)
+
+	SDSnames <- unlist(lapply(SDSnames,function(x) strsplit(x,"=")[[1]][2]))
+
+	sds <- unlist(lapply(SDSnames,function(x) {
+			x <- strsplit(x,":")[[1]]
+			x <- x[length(x)]					
+			}))	
+
+} else if (method=="mrt"){
+
+	if (missing(MRTpath)) {
+		MRTpath <- getPATH(quiet=TRUE)
+		}
+
+	if (!file.exists(MRTpath)) {stop("'MRTpath' is wrong or MRT not installed? Provide a good path, leave empty or run 'getPATH()' first!")}
+	
+	if (.Platform$OS=="unix"){
 	sdsRaw <- system(paste(file.path(MRTpath,"sdslist",fsep=fsep),HdfName,sep=" "),intern=TRUE)
 	
-}else if (.Platform$OS=="windows"){
-	sdsRaw <- shell(gsub(fsep,"\\\\",paste(file.path(MRTpath,"sdslist",fsep=fsep),HdfName,sep=" ")),intern=TRUE)
+	}else if (.Platform$OS=="windows"){
+		sdsRaw <- shell(gsub(fsep,"\\\\",paste(file.path(MRTpath,"sdslist",fsep=fsep),HdfName,sep=" ")),intern=TRUE)
 
-} else {
-	stop(cat("What OS have you? Please tell me so I can fix this.\n")) 
-}
+	} else {
+		stop(cat("What OS have you? Please tell me so I can fix this.\n")) 
+	}
 
-sds <- list()
-for (i in 1:length(sdsRaw)){
-sds[[i]] <- substr(sdsRaw[i],1,11) == "SDgetinfo: "
+	sds <- list()
+	for (i in 1:length(sdsRaw)){
+	sds[[i]] <- substr(sdsRaw[i],1,11) == "SDgetinfo: "
+	}
+	sds <- sdsRaw[unlist(sds)]
+	sds <- unlist(lapply(sds,function(x){strsplit(x,", ")[[1]][2]}))
 }
-sds <- sdsRaw[unlist(sds)]
-sds <- unlist(lapply(sds,function(x){strsplit(x,": ")[[1]][2]}))
-sds <- unlist(lapply(sds,function(x){paste(strsplit(x,", ")[[1]][1:2],collapse=": ")}))
 
 if (!missing(SDSstring)){
 	if (inherits(SDSstring,"list")) {
@@ -50,7 +79,19 @@ SDSstring <- gsub(pattern=" ",replacement="",x=SDSstring) # collapse the spaces
 	for (o in 1:length(sds)){
 	msk[o] <- substr(SDSstring,o,o)==1
 	}
-return(list(SDSnames = sds[msk],SDSstring = paste(as.numeric(msk),collapse=" ")))
+
+	if (method=="gdal") {
+	return(list(SDSnames = sds[msk],SDSstring = paste(as.numeric(msk),collapse=" "),SDS4gdal=SDSnames[msk]))
 	} else {
-return(sds)}
+	return(list(SDSnames = sds[msk],SDSstring = paste(as.numeric(msk),collapse=" ")))
+	}
+} else {
+
+	if (method=="gdal") {
+	return(list(SDSnames = sds,SDS4gdal=SDSnames))
+	} else {
+	return(list(SDSnames = sds))
+	}
 }
+}
+
