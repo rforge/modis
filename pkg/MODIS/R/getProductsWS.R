5160a03ad6c63625744550f8fb1aa0cb78ff34cb
janvb <janvb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-22 17:36:42 +0000

    adds regarding modis webservice
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modis@48 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/MODIS/R/getProductsWS.R
@@ -0,0 +1,18 @@
+# Author: Jan Verbesselt, Jan.Verbesselt@wur.nl
+# Date : December 2011
+# Licence GPL v3
+
+getProducts.WS <- function() {
+	## get the SOAP service
+	ornlMODIS = processWSDL("http://daac.ornl.gov/cgi-bin/MODIS/GLBVIZ_1_Glb_subset/MODIS_webservice.wsdl")
+	## define the function set
+	ornlMODISFuncs = genSOAPClientInterface(operations=ornlMODIS@operations[[1]], def=ornlMODIS)
+	## get the products
+	return(ornlMODISFuncs@functions$getproducts())
+}
+
+# getProducts.WS()
+
+
+
+
