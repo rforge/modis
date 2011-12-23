edd969b1bdb02df0c7921c72bb24f7d0709b7d0f
janvb <janvb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-23 17:15:01 +0000

    modis webservice update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modis@60 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/MODIS/R/getDatesWS.R
@@ -0,0 +1,18 @@
+# Author: Jan Verbesselt, Jan.Verbesselt@wur.nl
+# Date : December 2011
+# Licence GPL v3
+
+getDatesWS <- function(lat,long,product) {
+	## get the SOAP service
+	ornlMODIS = processWSDL("http://daac.ornl.gov/cgi-bin/MODIS/GLBVIZ_1_Glb_subset/MODIS_webservice.wsdl")
+	## define the function set
+	ornlMODISFuncs = genSOAPClientInterface(operations=ornlMODIS@operations[[1]], def=ornlMODIS)
+	## get the products
+	return(ornlMODISFuncs@functions$getdates(lat,long,product))
+}
+
+
+
+
+
+
