06e1311268afad43589450e832ece32c3120d38f
mattiuzzi5114 <mattiuzzi5114@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-23 10:36:01 +0000

    enabled support for YYYYDDD (doy of the year: i.e "2005001") output is now always yy-mm-dd and yyyyddd for begin- and for enddate
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modis@55 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/MODIS/R/transDATE.R
+++ pkg/MODIS/R/transDATE.R
@@ -12,6 +12,17 @@ if (is.null(end)) {
 	end="2025.12.31" # we have to remember or change it when the time has come ;)
 } 
 
+if (nchar(begin)==7) {
+Byear  <- substr(begin,1,4)
+Bdoy   <- substr(begin,5,7)
+begin  <- format(as.Date(as.numeric(Bdoy)-1,origin=paste(Byear,"-01-01",sep="")),"%Y.%m.%d")   
+}
+if (nchar(end)==7) {
+Eyear  <- substr(end,1,4)
+Edoy   <- substr(end,5,7)
+end    <- format(as.Date(as.numeric(Edoy)-1,origin=paste(Eyear,"-01-01",sep="")),"%Y.%m.%d")  
+}
+
 divisor <- substr(begin,5,5)
 begin   <- as.Date(begin,format=paste("%Y",divisor,"%m",divisor,"%d",sep=""))
 if (is.na(begin)) {stop("\n'begin=",begin,"' is eighter wrong format (not:'YYYY.MM.DD') or a invalid date")}
@@ -25,7 +36,14 @@ begin <- end
 end <- t
 rm(t)
 }
-return(list(begin=begin,end=end))
+
+beginDOY <- format(as.Date(begin,format="%Y.%m.%d"), "%Y%j")
+endDOY   <- format(as.Date(end,format="%Y.%m.%d"), "%Y%j")
+
+return(list(begin=begin,end=end,beginDOY=beginDOY,endDOY=endDOY))
 }
+
+
+
 
 
