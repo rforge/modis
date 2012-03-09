6800a32a637c80c9ae9a8d35b9b2f34833cf5e34
mattiuzzi5114 <mattiuzzi5114@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-03-09 17:28:22 +0000

    last little bugs to solve!
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modis@127 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/MODIS/R/getHdf.R
+++ pkg/MODIS/R/getHdf.R
@@ -86,11 +86,11 @@ return(invisible(unlist(dates)))
 	tLimits <- transDate(begin=begin,end=end)
 	#########
 	# getStruc
-	.getStruc(localArcPath=localArcPath,product=product,begin=tLimits$begin,end=tLimits$end,wait=0)
-
+	MODIS:::.getStruc(localArcPath=localArcPath,product=product,begin=tLimits$begin,end=tLimits$end,wait=0)
+	#.getStruc(localArcPath=localArcPath,product=product,server="LAADS",begin=tLimits$begin,end=tLimits$end,wait=0)
 	ftpdirs <- list()
 	ftpdirs[[1]] <- read.table(file.path(auxPATH,"LPDAAC_ftp.txt",fsep="/"),stringsAsFactors=FALSE)
-	ftpdirs[[2]] <- read.table(file.path(auxPATH,"LAADS_ftp.txt",fsep="/"),stringsAsFactors=FALSE)
+	#ftpdirs[[2]] <- read.table(file.path(auxPATH,"LAADS_ftp.txt",fsep="/"),stringsAsFactors=FALSE)
 
 	dates  <- list()
 	output <- list() # path info for the invisible output
@@ -98,162 +98,164 @@ return(invisible(unlist(dates)))
 		
 	for(z in 1:length(product$PRODUCT)){ # Platforms MOD/MYD
 
-		todo <- paste(product$PRODUCT[z],".",product$CCC[[which(names(product$CCC)==product$PRODUCT[z])]],sep="")
-		
 		if (product$TYPE[z]=="Swath") {
 			cat("'Swath'-products not yet supported, yumping to the next.\n")
 		}else{
 
-			# tileID
-			if (product$TYPE[z]=="CMG") {
-				tileID="GLOBAL"
-				ntiles=1 
-			} else {
-				if (!missing(extent)) {
-  					tileID <- getTile(extent=extent)$tile
- 				 } else if (!missing(tileH) & !missing(tileV)) {
-    				tileID <- getTile(tileH=tileH,tileV=tileV)$tile
- 				 } else {stop("Please provide eighter a 'tileH' plus 'tileV' or an 'extent'")}
-				ntiles <- length(tileID)
-			}
-
-			us <- lapply(ftpdirs,function(x){
-					x <- as.Date(x,format="%Y.%m.%d")
-					x <- sel >= tLimits$begin & x <= tLimits$end
-			}) # convert to date
-	
-			if (sum(us[[1]],na.rm=TRUE)>0){ 
-
-				dates[[z]] <- ftpdirs[us[[1]]]
-
-				dates[[z]] <- cbind(dates[[z]],matrix(rep(NA, length(dates[[z]])*ntiles),ncol=ntiles,nrow=length(dates[[z]])))
-				colnames(dates[[z]]) <- c("date",tileID)
-
-				for (i in 1:nrow(dates[[z]])){
-	
-					year <- format(as.Date(dates[[z]][i,1],format="%Y.%m.%d"), "%Y")
-					doy  <- as.integer(format(as.Date(dates[[z]][i,1],format="%Y.%m.%d"), "%j"))
-					doy  <- sprintf("%03d",doy)
-					datu <- paste("A",year,doy,sep="")
-					mtr  <- rep(1,ntiles) # for file availability flaging
-
-					# creates local directory (HDF file container)
-					path <- MODIS:::.genString()
-					arcPath <- paste(localArcPath,"/",product$PF2[z],product$PD,".",collection,"/",dates[[z]][i,1],"/",sep="")
-dir.create(arcPath,showWarnings=FALSE,recursive=TRUE)
-
-for(j in 1:ntiles){
-
-dates[[z]][i,j+1] <- paste(product$PF2[z],product$PD,".",datu,".",if (tileID[j]!="GLOBAL") {paste(tileID[j],".",sep="")},collection,".*.hdf$",sep="") # create pattern
-	
-	if (length(dir(arcPath,pattern=dates[[z]][i,j+1]))>0){ # if available locally
+			todo <- paste(product$PRODUCT[z],".",product$CCC[[which(names(product$CCC)==product$PRODUCT[z])]],sep="")
 		
-		HDF <- dir(arcPath,pattern=dates[[z]][i,j+1])  # extract HDF file
+			for (u in 1:length(todo)){
 		
-		if (length(HDF)>1) { # in very recent files sometimes there is more than 1 file/tile/date if so get the last
-			select <- list()
-			for (d in 1:length(HDF)){ 
-			select[[d]]<- strsplit(HDF[d],"\\.")[[1]][5]
-			}
-			HDF <- HDF[which.max(unlist(select))]		
-			}
-	dates[[z]][i,j+1] <- HDF
-	mtr[j] <- 0
-	}
-}
-
-
-if (sum(mtr)!=0) { # if one or more of the tiles in date is missing, its necessary to go on ftp
-
-	if(exists("ftpfiles")) {rm(ftpfiles)}
-	require(RCurl)
-	for (g in 1:sturheit){
-		try(ftpfiles <- getURL(paste(ftp,dates[[z]][i,1],"/",sep="")),silent=TRUE)
-		if(exists("ftpfiles")){break}
-	}
-	ftpfiles <- strsplit(ftpfiles, if(.Platform$OS.type=="unix"){"\n"} else{"\r\n"})[[1]]
-		if (wait > 0){Sys.sleep(as.numeric(wait))}
-
-	if (ftpfiles[1] != "total 0") {
-    
-    ftpfiles <- unlist(lapply(strsplit(ftpfiles," "),function(x){x[length(x)]})) # found empty dir!
+				# tileID
+				if (product$TYPE[z]=="CMG") {
+					tileID="GLOBAL"
+					ntiles=1 
+				} else {
+					if (!missing(extent)) {
+  						tileID <- getTile(extent=extent)$tile
+ 					 } else if (!missing(tileH) & !missing(tileV)) {
+    					tileID <- getTile(tileH=tileH,tileV=tileV)$tile
+ 					 } else {stop("Please provide eighter a 'tileH' plus 'tileV' or an 'extent'")}
+					ntiles <- length(tileID)
+				}
 	
-		for(j in 1:ntiles){
-		
-		if(mtr[j]==1){ # if tile is missing get it
-			onFtp <- grep(ftpfiles,pattern=dates[[z]][i,j+1],value=TRUE)
-			HDF   <- grep(onFtp,pattern=".hdf$",value=TRUE)
+				# us <- lapply(ftpdirs,function(x){
+				#		x <- as.Date(x,format="%Y.%m.%d")
+				#		x <- x >= tLimits$begin & x <= tLimits$end
+				# }) # convert to date
+				
+				datedirs <- ftpdirs[[1]][,todo[u]]
+				datedirs <- datedirs[!is.na(datedirs)]			
+				sel <- as.Date(datedirs,format="%Y.%m.%d")
+				us  <- sel >= tLimits$begin & sel <= tLimits$end
+				
+				if (sum(us,na.rm=TRUE)>0){ 
+				
+					l=l+1
 
-			if(length(HDF)>0){
 					
-				if (length(HDF)>1) { # in very recent files sometimes there is more than 1 file/tile/date if so get the last
-				select <- list()
-				for (d in 1:length(HDF)){
-				select[[d]]<- strsplit(HDF[d],"\\.")[[1]][5]
-				}
-				HDF <- HDF[which.max(unlist(select))]		
-				}
-			dates[[z]][i,j+1] <- HDF
-				
+					dates[[l]] <- datedirs[us]
 
-			g=1
-			while(g <= sturheit) {
+					dates[[l]] <- cbind(dates[[l]],matrix(rep(NA, length(dates[[l]])*ntiles),ncol=ntiles,nrow=length(dates[[l]])))
+					colnames(dates[[l]]) <- c("date",tileID)
+
+					for (i in 1:nrow(dates[[l]])){
+	
+						year <- format(as.Date(dates[[l]][i,1],format="%Y.%m.%d"), "%Y")
+						doy  <- as.integer(format(as.Date(dates[[l]][i,1],format="%Y.%m.%d"), "%j"))
+						doy  <- sprintf("%03d",doy)
+						datu <- paste("A",year,doy,sep="")
+						mtr  <- rep(1,ntiles) # for file availability flaging
+	
+						# creates local directory (HDF file container)
+						path <- MODIS:::.genString(x=strsplit(todo[u],"\\.")[[1]][1],collection=strsplit(todo[u],"\\.")[[1]][2],date=dates[[l]][i,1])
+	
+						for(j in 1:ntiles){
+	
+							dates[[l]][i,j+1] <- paste(product$PF2[z],product$PD,".",datu,".",if (tileID[j]!="GLOBAL") {paste(tileID[j],".",sep="")},collection,".*.hdf$",sep="") # create pattern
+		
+								if (length(dir(path$localPath,pattern=dates[[l]][i,j+1]))>0){ # if available locally
 			
-				if (g==1){qi <- quiet} else {qi <- TRUE}
-				hdf=1
-				try(hdf <- download.file(paste(ftp, dates[[z]][i,1], "/", HDF,sep=""),
-					destfile=paste(arcPath, HDF, sep=""), mode='wb', method=dlmethod, quiet=qi, cacheOK=FALSE),silent=TRUE)
-				if(hdf==0 & !quiet) {
-					if (g==1) {
-						cat("Downloaded by the first try!\n\n")
-					} else {
-						cat("Downloaded after",g,"retries!\n\n")
-					}
-				}
-				if(hdf==0) {break}
-			g=g+1
-			}
+									HDF <- dir(path$localPath,pattern=dates[[l]][i,j+1])  # extract HDF file
+			
+									if (length(HDF)>1) { # in very recent files sometimes there is more than 1 file/tile/date if so get the last
+										select <- list()
+										for (d in 1:length(HDF)){ 
+											select[[d]]<- strsplit(HDF[d],"\\.")[[1]][5]
+										}
+										HDF <- HDF[which.max(unlist(select))]		
+									}
+									dates[[l]][i,j+1] <- HDF
+									mtr[j] <- 0
+								}
+						}
+
+						if (sum(mtr)!=0) { # if one or more of the tiles in date is missing, its necessary to go on ftp
+
+							if(exists("ftpfiles")) {rm(ftpfiles)}
+							require(RCurl) # get the filenames
+							
+							for (g in 1:sturheit){ # get list of files in remote dir
+								try(ftpfiles <- getURL(paste(path$remotePath[[(g%%length(path$remotePath))+1]],"/",sep="")),silent=TRUE)
+								if(exists("ftpfiles")){break}
+								Sys.sleep(as.numeric(wait))
+							}
+							if(!exists("ftpfiles")) {stop("Problems with FTP connections try a little later")} # This breaks the entire job! it schouldn't, better to jump to the next file...may it is local!
+			
+							ftpfiles <- strsplit(ftpfiles, if(.Platform$OS.type=="unix"){"\n"} else{"\r\n"})[[1]]
+	
+							if (ftpfiles[1] != "total 0") {
+	    
+								ftpfiles <- unlist(lapply(strsplit(ftpfiles," "),function(x){x[length(x)]})) # found empty dir!
+		
+								for(j in 1:ntiles){
 				
-			mtr[j] <- hdf
-				if (wait > 0){Sys.sleep(as.numeric(wait))}
-			} else { 
-				dates[[z]][i,j+1] <- "No tile for location" 
-			}
-		} # if mtr==1
-	
+									if(mtr[j]==1){ # if tile is missing get it
+										onFtp <- grep(ftpfiles,pattern=dates[[l]][i,j+1],value=TRUE)
+										HDF   <- grep(onFtp,pattern=".hdf$",value=TRUE)
+		
+										if(length(HDF)>0){
+					
+											if (length(HDF)>1) { # in very recent files sometimes there is more than 1 file/tile/date if so get the last
+												select <- list()
+												for (d in 1:length(HDF)){
+													select[[d]] <- strsplit(HDF[d],"\\.")[[1]][5]
+												}
+												HDF <- HDF[which.max(unlist(select))]		
+											}
+											dates[[l]][i,j+1] <- HDF
+
+											for(g in 1:sturheit) {
+												server <- c("LAADS","LPDAAC")[g%%length(path$remotePath)+1]
+												cat("Getting file from:",server,"\n")
+												hdf=1
+												try(hdf <- download.file(
+													paste(path$remotePath[[(g%%length(path$remotePath))+1]],"/", HDF,sep=""),
+													destfile=paste(path$localPath,"/", HDF, sep=""),
+													mode='wb', method=dlmethod, quiet=quiet, cacheOK=FALSE),
+												silent=TRUE)
+												if(hdf==0 & !quiet) {
+													if (g==1) {
+														cat("Downloaded by the first try!\n\n")
+													} else {
+														cat("Downloaded after",g,"retries!\n\n")
+													}
+												}
+												if(hdf==0) {break}
+											}
+					
+											mtr[j] <- hdf
+											Sys.sleep(as.numeric(wait))
+										} else { 
+											dates[[l]][i,j+1] <- "No tile for location" 
+										}
+									} # if mtr==1
+								}
+							} else {
+								dates[[l]][i,(j+1):ncol(dates[[l]])] <- "No files for that date on FTP"
+							} # on ftp is possible to find empty folders!
+						}
+					}
+					if (log) {
+						dir.create(file.path(localArcPath,"LOGS",fsep="/"),showWarnings=FALSE)	
+						write.csv(dates[[l]],file=file.path(localArcPath,"LOGS",paste(todo[u],"_LOG.csv",sep=""),fsep="/"))
+					}
+		
+					if(checkSize){
+						for (g in 1:sturheit){ 
+							try(xmlIn <- getXml(HdfName = as.list(paste(path$localPath,"/",dates[[l]]		[i,-1],sep="")),wait=wait,quiet=qi,dlmethod=dlmethod),silent=TRUE)
+							if(sum(xmlIn)==0) {break}
+						}
+					}
+					output[[l]] <- paste(path$localPath,"/",grep(dates[[l]][i,-1],pattern=".hdf$",value=TRUE),sep="")
+					names(output)[l] <- todo[u]
+				} else {
+					cat(paste("No files on ftp in date range for: ",todo[u],"\n\n",sep=""))
+				}
+			} 
 		}
-	} else {
-	dates[[z]][i,(j+1):ncol(dates[[z]])] <- "No files for that date on FTP"
-	} # on ftp is possible to find empty folders!
-}
-
-	if (log) {
-		dir.create(file.path(localArcPath,"LOGS",fsep="/"),showWarnings=FALSE)	
-		write.csv(dates[[z]],file=file.path(localArcPath,"LOGS",paste(product$PF2[z],product$PD,".",collection,"_LOG.csv",sep=""),fsep="/"))
 	}
-	
-	if(checkSize){
-		g=1
-		while(g <= sturheit) {
-			if (g==1){qi <- quiet} else { qi <- TRUE}
-			try(xmlIn <- getXml(HdfName = as.list(paste(arcPath,dates[[z]][i,-1],sep="")),wait=wait,quiet=qi,dlmethod=dlmethod),silent=TRUE)
-		if(sum(xmlIn)==0) {break}
-		g=g+1
-		}
-	} # as.list() should not be needed but but but...
-
-l=l+1
-output[[l]] <- paste(arcPath,grep(dates[[z]][i,-1],pattern=".hdf$",value=TRUE),sep="")
-} # end dates i 
-
-}else{
-#		if (!quiet){
-	cat(paste("No files on ftp in date range for: ",product$PF2[z],product$PD,".",collection,"\n\n",sep=""))
-#		}
-	}
-} # if no files are avalaible for product in date AND end platform z
-}
-return(invisible(paste(unlist(output),sep="")))
-} # end if not HdfName
+	return(invisible(output))
+} 
 } ## END: FTP vs ARC check and download 
 
