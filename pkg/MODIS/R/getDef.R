16caeea8deec5ef0faed1f13361f8b6f8c75d545
mattiuzzi5114 <mattiuzzi5114@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-12-03 08:02:06 +0000

    still not working. makeOpts has problems with lazy load!
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modis@351 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/MODIS/R/getDef.R
+++ pkg/MODIS/R/getDef.R
@@ -1,45 +1,44 @@
-# Internal function to gatter Package defaults
+# Internal function to gatter package defaults
 
 .getDef <- function(...)
 {
-    MODIS:::makeOpts(changeStruc=TRUE)
-    
-    defOpts <- new.env()
-    eval(parse(file.path("~/.MODIS_Opts.R")),envir=defOpts)
-    opt2 <- as.list(defOpts)    
-    
-    try(testDir <- normalizePath(opt2$localArcPath,"/",mustWork = TRUE), silent = TRUE)
-    if(!exists("testDir")) 
-    {
-        warning("'localArcPath' not found, it will be created!")
-        localArcPath <- normalizePath(opt2$localArcPath, "/", mustWork = FALSE)
-        dir.create(localArcPath, recursive = TRUE, showWarnings = TRUE)
-    }
-    
-    try(testDir <- normalizePath(opt2$outDirPath, "/", mustWork=TRUE), silent = TRUE)
-    if(!exists("testDir")) 
-    {
-        warning("'outDirPath' not found, it will be created!")
-        outDirPath <- normalizePath(opt2$outDirPath, "/", mustWork = FALSE)
-        dir.create(outDirPath, recursive = TRUE, showWarnings = TRUE)
-    }
-    
-    opt1 <- list(...) # if only a subset of opts should be printed
+  opt1 <- list(...) # if only a subset of opts should be printed
+  
+  if (file.exists("~/.MODIS_Opts.R"))
+  {
+    # this corrects/modifies (currently only 'arcStructure') ".MODIS_opts.R" file.
+    # (It also creates the MODIS_opts.R file)
+    makeOpts(changeStruc=TRUE)
+  }
+  
+  localArcPath <- normalizePath(MODIS:::MODISpackageOpts$localArcPath, "/", mustWork = FALSE)
+  if(length(dir(localArcPath))==0) 
+  {
+      warning("'localArcPath' not found, it will be created!")
+      dir.create(localArcPath, recursive = TRUE, showWarnings = TRUE)
+  }
 
+  outDirPath <- normalizePath(MODIS:::MODISpackageOpts$outDirPath, "/", mustWork = FALSE)  
+  if(length(dir(outDirPath))==0)
+  {
+      warning("'outDirPath' not found, it will be created!")
+      dir.create(outDirPath, recursive = TRUE, showWarnings = TRUE)
+  }
+  
 	if (length(opt1)==0)
-	{
-    	return(opt2)
+  {
+  	return(MODISpackageOpts)
 	} else 
 	{
 		result <- list()
 	
 		for (i in 1:length(opt1))
 		{
-			result[[i]] <- opt2[[grep(names(opt2),pattern=opt1[[i]])]]
+			result[[i]] <- MODISpackageOpts[[grep(names(MODISpackageOpts),pattern=opt1[[i]])]]
 		}
 		names(result) <- opt1
 
-	    return(if(length(result)==1) {unlist(result)} else {result})
+	  return(if(length(result)==1) {unlist(result)} else {result})
 	}
 }
 
