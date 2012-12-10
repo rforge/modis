ae34555e94908ab37e4708d5068934d846f19c32
mattiuzzi5114 <mattiuzzi5114@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-12-10 16:08:53 +0000

    coding error
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modis@364 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/MODIS/R/doOptions.R
+++ pkg/MODIS/R/doOptions.R
@@ -194,7 +194,7 @@ combineOptions <- function(...)
 {
     opts <- options() # collects all defaults
     opts <- opts[grep(names(opts),pattern="^MODIS_*.")] # isolate MODIS_opts
-    if(length(opts)==0 | !file.exists("~/.MODIS_opts.R"))
+    if(length(opts)==0 | !file.exists("~/.MODIS_Opts.R"))
     {
         MODISoptions()    
         opts <- options() # collects all defaults
