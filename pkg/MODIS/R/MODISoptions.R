19d70553edac6b49173af38fb6d53a7ab3928d85
mattiuzzi5114 <mattiuzzi5114@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-12-16 10:42:18 +0000

    .
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modis@372 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/MODIS/R/MODISoptions.R
+++ pkg/MODIS/R/MODISoptions.R
@@ -24,7 +24,7 @@ MODISoptions <- function(localArcPath, outDirPath, pixelSize, outProj, resamplin
     eval(parse(file.path(find.package("MODIS"), "external", "MODIS_Opts.R")),envir=opts) 
     
     # 2. system wide
-    sysopts <- paste(R.home(component="etc"), '/', 'MODIS_opts.R', sep='')
+    sysopts <- paste(R.home(component="etc"), '/', '.MODIS_opts.R', sep='')
     so      <- FALSE
     
     if (file.exists(sysopts))
