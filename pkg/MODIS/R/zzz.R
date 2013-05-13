4686192737594420ed438c28491cf3df0011a69d
mattiuzzi5114 <mattiuzzi5114@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-05-13 09:30:08 +0000

    separated package option setting (to .onLoad) from 'packageStartupMessage("MODIS Manual...' (to .onAttach). I hope it is good as it is now.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modis@391 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/MODIS/R/zzz.R
+++ pkg/MODIS/R/zzz.R
@@ -4,9 +4,10 @@
     options(warn=-1)
     MODISoptions(save=FALSE, checkPackages=FALSE, quiet=TRUE)
     options(warn=win$warn)
-
+}
+.onAttach <- function(lib, pkg)
+{
 	packageStartupMessage("MODIS_manual: http://ivfl-arc.boku.ac.at/owncloud/public.php?service=shorty_relay&id=QR5FZSxe0g")
-
 }
 
 
