312c9e2d8e54adfc95a8df26e2cc8faf1daf506c
jmaspons <jmaspons@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-19 11:32:05 +0000

    Check the data before to create the result
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modis@460 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/modisws/R/createbrickWS.R
+++ pkg/modisws/R/createbrickWS.R
@@ -6,7 +6,7 @@
 ## not sure what the best way is here
 ## to deal with helper functions
 
-createbrickWS <- function(result, lat, lon, product, band, dMODIS) 
+createbrickWS <- function(result) 
 {  
 
   ## set raster
@@ -15,29 +15,29 @@ createbrickWS <- function(result, lat, lon, product, band, dMODIS)
   nrows <- result@nrows  
   ncols <- result@ncols  
   nl    <- length(result@subset) # nl <- dim(summary(ddd))[1]
-  
-  dd <- strsplit(result@subset,"\n") # is the form of result@subset the only option to get the values?
-  
-  # modd by Joan Maspons
-  dd <- sapply(dd, function(x)  
-  {
-    x <- strsplit(x, ",")
-    if (length(x) > 0)
-    {
-      x <- as.numeric(x[[1]][-1:-5])
-      if (length(x) != nrows * ncols)
-      {
-        warning("Incorrect MODIS data for", lat, lon, product, band, "\n\t", x)
-        x <- rep(dMODIS$NAsMax[dMODIS$band == band], nrows * ncols)
-      }
-    } else
-    {
-      warning("The invalid server response")
-      x <- rep(NA, nrows * ncols)
-    }
-    return(x)
-  })
 
+  dd <- strsplit(result@subset,"\n") # is the form of result@subset the only option to get the values?
+
+  # modd by Joan Maspons
+  if (length(unlist(dd)) == 0){ # No data
+    dd <- matrix(nrow=nrows * ncols, ncol=nl)
+    warning("No data for these coordinates. Latitude Longitude: ", result@latitude, " ", result@longitude) ##TODO: error vs warning?
+
+  }else{
+    dd <- sapply(dd, function(x){
+        dat <- strsplit(x, ",")
+        dat <- as.numeric(dat[[1]][-1:-5])
+
+        if (length(dat) != nrows * ncols){
+          warning("Incorrect MODIS data:\n\t", x) ##TODO: error vs warning?
+          dat <- rep(NA, nrows * ncols)
+        }
+
+        return(dat)
+      })
+
+  }
+  
   #dd <- sapply(dd,function(x){as.numeric(strsplit(x,",")[[1]][-1:-5])})
   #dd <- dd * result@scale
   
@@ -53,7 +53,22 @@ createbrickWS <- function(result, lat, lon, product, band, dMODIS)
   return(res) # brick or raster depends on the layers
 }
 
+## TEST
+# ornlMODIS = processWSDL("http://daac.ornl.gov/cgi-bin/MODIS/GLBVIZ_1_Glb_subset/MODIS_webservice.wsdl")
+# ornlMODISFuncs = genSOAPClientInterface(operations = ornlMODIS@operations[[1]], def = ornlMODIS)
 
+# lat<- 41.38879
+# lon<-   2.15899
+
+# lat0<- lon0<- 0 # incorrect data
+#
+# dates<- getDatesWS(lat, lon, "MOD13Q1")
+# radi<- 10
+# result1 = ornlMODISFuncs@functions$getsubset(lat, lon, "MOD13Q1", "250m_16_days_NDVI", dates[1], dates[2], radi, radi)
+# result0<- ornlMODISFuncs@functions$getsubset(lat0, lon0, "MOD13Q1", "250m_16_days_NDVI", dates[1], dates[2], radi, radi)
+
+
+##TODO remove?
 # createbrickWS <- function(result) {  
 #   ## set raster
 #   modisprj <-c("+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs")
