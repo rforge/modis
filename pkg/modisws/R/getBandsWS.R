de220bf13a1114ae1c695d5cf364d3be0f97cf1d
jmaspons <jmaspons@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-12 18:49:27 +0000

    Get the SOAP service and define the function set at loading time instead of at every function call. Require SOAP package at loading time to avoid problems when building the package at R-forge
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modis@465 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/modisws/R/getBandsWS.R
+++ pkg/modisws/R/getBandsWS.R
@@ -3,12 +3,6 @@
 # Licence GPL v3
 
 getBandsWS <- function(product) {
-
-	## get the SOAP service
-	ornlMODIS = processWSDL("http://daac.ornl.gov/cgi-bin/MODIS/GLBVIZ_1_Glb_subset/MODIS_webservice.wsdl")
-	## define the function set
-	ornlMODISFuncs = genSOAPClientInterface(operations=ornlMODIS@operations[[1]], def=ornlMODIS)
-	## get the products
 	return(ornlMODISFuncs@functions$getbands(product))
 }
 
--- pkg/modisws/R/getDatesWS.R
+++ pkg/modisws/R/getDatesWS.R
@@ -2,12 +2,6 @@
 # Date : December 2011
 
 getDatesWS <- function(lat,long,product) {
-
-	## get the SOAP service
-	ornlMODIS = processWSDL("http://daac.ornl.gov/cgi-bin/MODIS/GLBVIZ_1_Glb_subset/MODIS_webservice.wsdl")
-	## define the function set
-	ornlMODISFuncs = genSOAPClientInterface(operations=ornlMODIS@operations[[1]], def=ornlMODIS)
-	## get the products
 	return(ornlMODISFuncs@functions$getdates(lat,long,product))
 }
 
