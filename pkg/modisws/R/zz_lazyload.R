0a99168a5e1d4727bb123d2b32d8835b04589fbb
jmaspons <jmaspons@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-31 10:09:14 +0000

    Don't require SSOAP package but print a message if it is not present and warn that the package isn't ready.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modis@467 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/modisws/R/zz_lazyload.R
+++ pkg/modisws/R/zz_lazyload.R
@@ -1,11 +1,10 @@
 if (!require(SSOAP)){
-  stop("You need to install the 'SSOAP' package: \n\tinstall.packages('SSOAP', repos = 'http://www.omegahat.org/R', dependencies=TRUE, type='source')")
+  message("modisws NOT READY. You need to install the 'SSOAP' package: \n\tinstall.packages('SSOAP', repos = 'http://www.omegahat.org/R', dependencies=TRUE, type='source')")
+  # STOP: package not ready
+}else{
+  ## get the SOAP service
+  ornlMODIS<- processWSDL("http://daac.ornl.gov/cgi-bin/MODIS/GLBVIZ_1_Glb_subset/MODIS_webservice.wsdl")
+
+  ## define the function set
+  ornlMODISFuncs<- genSOAPClientInterface(operations=ornlMODIS@operations[[1]], def=ornlMODIS)
 }
-
-## get the SOAP service
-ornlMODIS<- processWSDL("http://daac.ornl.gov/cgi-bin/MODIS/GLBVIZ_1_Glb_subset/MODIS_webservice.wsdl")
-
-## define the function set
-ornlMODISFuncs<- genSOAPClientInterface(operations=ornlMODIS@operations[[1]], def=ornlMODIS)
-
-# load(system.file("external", "ornlMODISFuncs.RData", package="modisws"))
